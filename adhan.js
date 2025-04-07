47804a2020f7f6912605245e5ed6401905a556ed
FaridBou <95292449+FaridBou@users.noreply.github.com>
2025-04-07 07:23:05 +0200

    Add files via upload

--- adhan.js
+++ adhan.js
@@ -1,7 +1,34 @@
 
-/*!
- * Adhan JavaScript Library v4.0.0 (UMD build)
- * Copyright (c) 2023 Batoul Apps
- * https://github.com/batoulapps/adhan-js
- */
-(function(global,factory){typeof exports==="object"&&typeof module!=="undefined"?module.exports=factory():typeof define==="function"&&define.amd?define(factory):(global.adhan=factory());})(this,function(){"use strict";return {/* Librairie Adhan ici (abrégée pour la démo) */};});
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.adhan = factory());
+})(this, (function () {
+  'use strict';
+  return {
+    CalculationMethod: {
+      Other: function () {
+        return {
+          fajrAngle: 18,
+          ishaAngle: 17,
+          maghribAngle: null
+        };
+      }
+    },
+    Coordinates: function(lat, lon) {
+      this.latitude = lat;
+      this.longitude = lon;
+    },
+    PrayerTimes: function(coords, date, params) {
+      const base = new Date(date);
+      const addMinutes = (d, m) => new Date(d.getTime() + m * 60000);
+      return {
+        fajr: addMinutes(base, 300),
+        dhuhr: addMinutes(base, 720),
+        asr: addMinutes(base, 900),
+        maghrib: addMinutes(base, 1080),
+        isha: addMinutes(base, 1200)
+      };
+    }
+  };
+}));
