187714e8734091f70c565c4a13e4bb7f18f175a4
FaridBou <95292449+FaridBou@users.noreply.github.com>
2025-04-07 07:13:19 +0200

    Add files via upload

---
+++ adhan.js
@@ -0,0 +1,7 @@
+
+/*!
+ * Adhan JavaScript Library v4.0.0 (UMD build)
+ * Copyright (c) 2023 Batoul Apps
+ * https://github.com/batoulapps/adhan-js
+ */
+(function(global,factory){typeof exports==="object"&&typeof module!=="undefined"?module.exports=factory():typeof define==="function"&&define.amd?define(factory):(global.adhan=factory());})(this,function(){"use strict";return {/* Librairie Adhan ici (abrégée pour la démo) */};});
