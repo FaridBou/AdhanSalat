69427c2a1d4eff8b1124f48e0950f9053b81a69b
FaridBou <95292449+FaridBou@users.noreply.github.com>
2025-04-07 12:20:38 +0200

    Update adhan.js

--- adhan.js
+++ adhan.js
@@ -3,9 +3,29 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global.adhan = factory());
-})(this, (function () {
+})(this, function () {
   'use strict';
+
+  function Coordinates(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  function PrayerTimes(coords, date, params) {
+    const baseTime = new Date(date.setHours(0, 0, 0, 0));
+    const offset = (h, m) => new Date(baseTime.getTime() + ((h * 60 + m) * 60000));
+    return {
+      fajr: offset(5, 0),
+      dhuhr: offset(13, 0),
+      asr: offset(16, 30),
+      maghrib: offset(19, 50),
+      isha: offset(21, 10)
+    };
+  }
+
   return {
+    Coordinates: Coordinates,
+    PrayerTimes: PrayerTimes,
     CalculationMethod: {
       Other: function () {
         return {
@@ -14,21 +34,6 @@
           maghribAngle: null
         };
       }
-    },
-    Coordinates: function(lat, lon) {
-      this.latitude = lat;
-      this.longitude = lon;
-    },
-    PrayerTimes: function(coords, date, params) {
-      const base = new Date(date);
-      const addMinutes = (d, m) => new Date(d.getTime() + m * 60000);
-      return {
-        fajr: addMinutes(base, 300),
-        dhuhr: addMinutes(base, 720),
-        asr: addMinutes(base, 900),
-        maghrib: addMinutes(base, 1080),
-        isha: addMinutes(base, 1200)
-      };
     }
   };
-}));
+});
