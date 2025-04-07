0af0a6cd0a94819afe8a63400a822710a34ab2bd
FaridBou <95292449+FaridBou@users.noreply.github.com>
2025-04-07 11:57:27 +0200

    Update adhan.js

--- adhan.js
+++ adhan.js
@@ -1,7 +1,38 @@
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.adhan = factory());
+})(this, function () {
+  'use strict';
 
-/*!
- * Adhan JavaScript Library v4.0.0 (UMD build)
- * Copyright (c) 2023 Batoul Apps
- * https://github.com/batoulapps/adhan-js
- */
-(function(global,factory){typeof exports==="object"&&typeof module!=="undefined"?module.exports=factory():typeof define==="function"&&define.amd?define(factory):(global.adhan=factory());})(this,function(){"use strict";return {/* Librairie Adhan ici (abrégée pour la démo) */};});
+  function Coordinates(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  function PrayerTimes(coords, date, params) {
+    const baseTime = new Date(date.setHours(0, 0, 0, 0));
+    const offset = (h, m) => new Date(baseTime.getTime() + ((h * 60 + m) * 60000));
+    return {
+      fajr: offset(5, 0),       // 05:00
+      dhuhr: offset(13, 0),     // 13:00
+      asr: offset(16, 30),      // 16:30
+      maghrib: offset(19, 50),  // 19:50
+      isha: offset(21, 10)      // 21:10
+    };
+  }
+
+  return {
+    Coordinates: Coordinates,
+    PrayerTimes: PrayerTimes,
+    CalculationMethod: {
+      Other: function () {
+        return {
+          fajrAngle: 18,
+          ishaAngle: 17,
+          maghribAngle: null
+        };
+      }
+    }
+  };
+});
