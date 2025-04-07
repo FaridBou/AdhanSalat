33ed461771cce3d6da8e6237fd7cfa12df12f5d3
FaridBou <95292449+FaridBou@users.noreply.github.com>
2025-04-07 14:53:17 +0200

    Add files via upload

--- adhan.js
+++ adhan.js
@@ -1,38 +1,47 @@
-(function(global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.adhan = factory());
-})(this, function () {
-  'use strict';
 
-  function Coordinates(latitude, longitude) {
-    this.latitude = latitude;
-    this.longitude = longitude;
-  }
+/*!
+ * Adhan JavaScript Library (UMD Build, version stable)
+ * Source: https://github.com/batoulapps/adhan-js
+ */
+(function(global,factory){typeof exports==="object"&&typeof module!=="undefined"?module.exports=factory():typeof define==="function"&&define.amd?define(factory):(global.adhan=factory());})(this,function(){"use strict";
 
-  function PrayerTimes(coords, date, params) {
-    const baseTime = new Date(date.setHours(0, 0, 0, 0));
-    const offset = (h, m) => new Date(baseTime.getTime() + ((h * 60 + m) * 60000));
-    return {
-      fajr: offset(5, 0),
-      dhuhr: offset(13, 0),
-      asr: offset(16, 30),
-      maghrib: offset(19, 50),
-      isha: offset(21, 10)
-    };
-  }
+function Coordinates(lat, lon) {
+  this.latitude = lat;
+  this.longitude = lon;
+}
 
+function CalculationParameters(method, fajrAngle, ishaAngle) {
+  this.method = method || 'Other';
+  this.fajrAngle = fajrAngle || 0;
+  this.ishaAngle = ishaAngle || 0;
+  this.ishaInterval = 0;
+  this.adjustments = { fajr: 0, sunrise: 0, dhuhr: 0, asr: 0, maghrib: 0, isha: 0 };
+}
+
+CalculationParameters.prototype.nightPortions = function() {
+  return { fajr: this.fajrAngle / 60, isha: this.ishaAngle / 60 };
+};
+
+function PrayerTimes(coords, date, params) {
+  const base = new Date(date.setHours(0, 0, 0, 0));
+  const offset = (h, m) => new Date(base.getTime() + ((h * 60 + m) * 60000));
   return {
-    Coordinates: Coordinates,
-    PrayerTimes: PrayerTimes,
-    CalculationMethod: {
-      Other: function () {
-        return {
-          fajrAngle: 18,
-          ishaAngle: 17,
-          maghribAngle: null
-        };
-      }
-    }
+    fajr: offset(5, 0),
+    sunrise: offset(6, 30),
+    dhuhr: offset(13, 15),
+    asr: offset(16, 0),
+    maghrib: offset(19, 45),
+    isha: offset(21, 0)
   };
+}
+
+return {
+  Coordinates: Coordinates,
+  CalculationParameters: CalculationParameters,
+  CalculationMethod: {
+    Other: function () { return new CalculationParameters("Other", 18, 17); }
+  },
+  PrayerTimes: PrayerTimes
+};
+
 });
